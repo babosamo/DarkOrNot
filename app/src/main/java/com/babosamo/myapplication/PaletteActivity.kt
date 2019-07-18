1fd7fcef39487e9bc332f5ed3098cd46cea62b3a
babosamo <babosamo@gmail.com>
2019-07-18 15:43:15 +0900

    forceDarkAllowed = false 뷰 예시 추가

--- app/src/main/java/com/babosamo/myapplication/PaletteActivity.kt
+++ app/src/main/java/com/babosamo/myapplication/PaletteActivity.kt
@@ -25,46 +25,28 @@ class PaletteActivity : AppCompatActivity() {
         initView()
     }
 
-    init {
-//        AppCompatResources.getColorStateList(this, R.color.abc_background_cache_hint_selector_material_dark)
-//        AppCompatResources.getThemeColor
-
-    }
+    init { }
 
     private fun initView() {
         button_6200EE.setOnClickListener {
-            makeColor(Color.parseColor("#6200ee"))
-            getThemeColor(R.color.c_6200EE)
         }
 
         button_3700B3.setOnClickListener {
-            makeColor(Color.parseColor("#3700b3"))
-            getThemeColor(R.color.c_3700B3)
         }
 
         button_03DAC6.setOnClickListener {
-            makeColor(Color.parseColor("#03DAC6"))
-            getThemeColor(R.color.c_03DAC6)
         }
 
         button_018786.setOnClickListener {
-            makeColor(Color.parseColor("#018786"))
-            getThemeColor(R.color.c_018786)
         }
 
         button_FFFFFF.setOnClickListener {
-            makeColor(Color.parseColor("#FFFFFF"))
-            getThemeColor(R.color.c_FFFFFF)
         }
 
         button_000000.setOnClickListener {
-            makeColor(Color.parseColor("#000000"))
-            getThemeColor(R.color.c_018786)
         }
 
         button_B00020.setOnClickListener {
-            makeColor(Color.parseColor("#B00020"))
-            getThemeColor(R.color.c_B00020)
         }
     }
 
@@ -89,72 +71,72 @@ class PaletteActivity : AppCompatActivity() {
         val themeColor = getThemeColor(this@PaletteActivity, id)
         val hexColor = String.format("#%06X", 0xFFFFFF and themeColor)
         Logger.log("themeColor alpha ${themeColor.alpha} $hexColor")
-        color_display.setBackgroundColor(themeColor)
+//        color_display.setBackgroundColor(themeColor)
     }
 
 
-    private fun makeColor(color: Int) {
-        val bitmap = createImage(color = color)
-        color_display.setBackgroundColor(color)
-
-
-        Palette.from(bitmap).generate {
-            Logger.log("vibrantSwatch ${it?.vibrantSwatch}")
-            it?.vibrantSwatch?.run {
-                vibrant.setBackgroundColor(rgb)
-                vibrant.text = rgb.toString()
-            } ?: run {
-                vibrant.setBackgroundColor(Color.TRANSPARENT)
-            }
-
-            Logger.log("darkVibrantSwatch ${it?.darkVibrantSwatch}")
-            it?.darkVibrantSwatch?.run {
-                vibrant_dark.setBackgroundColor(rgb)
-                vibrant_dark.text = rgb.toString()
-            } ?: run {
-                vibrant_dark.setBackgroundColor(Color.TRANSPARENT)
-            }
-
-            Logger.log("lightVibrantSwatch ${it?.lightVibrantSwatch}")
-            it?.lightVibrantSwatch?.run {
-                vibrant_light.setBackgroundColor(rgb)
-                vibrant_light.text = rgb.toString()
-            } ?: run {
-                vibrant_light.setBackgroundColor(Color.TRANSPARENT)
-            }
-
-            Logger.log("mutedSwatch ${it?.mutedSwatch}")
-            it?.mutedSwatch?.run {
-                muted.setBackgroundColor(rgb)
-                muted.text = rgb.toString()
-            } ?: run {
-                muted.setBackgroundColor(Color.TRANSPARENT)
-            }
-
-            Logger.log("darkMutedSwatch ${it?.darkMutedSwatch}")
-            it?.darkMutedSwatch?.run {
-                muted_dark.setBackgroundColor(rgb)
-                muted_dark.text = rgb.toString()
-            } ?: run {
-                muted_dark.setBackgroundColor(Color.TRANSPARENT)
-            }
-
-            Logger.log("lightMutedSwatch ${it?.lightMutedSwatch}")
-            it?.lightMutedSwatch?.run {
-                muted_light.setBackgroundColor(rgb)
-                muted_light.text = rgb.toString()
-            } ?: run {
-                muted_light.setBackgroundColor(Color.TRANSPARENT)
-            }
-        }
-    }
-
-    private fun createImage(width: Int = 50, height: Int = 50, color: Int): Bitmap {
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        val paint = Paint()
-        paint.color = color
-        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
-        return bitmap
-    }
+//    private fun makeColor(color: Int) {
+//        val bitmap = createImage(color = color)
+//        color_display.setBackgroundColor(color)
+//
+//
+//        Palette.from(bitmap).generate {
+//            Logger.log("vibrantSwatch ${it?.vibrantSwatch}")
+//            it?.vibrantSwatch?.run {
+//                vibrant.setBackgroundColor(rgb)
+//                vibrant.text = rgb.toString()
+//            } ?: run {
+//                vibrant.setBackgroundColor(Color.TRANSPARENT)
+//            }
+//
+//            Logger.log("darkVibrantSwatch ${it?.darkVibrantSwatch}")
+//            it?.darkVibrantSwatch?.run {
+//                vibrant_dark.setBackgroundColor(rgb)
+//                vibrant_dark.text = rgb.toString()
+//            } ?: run {
+//                vibrant_dark.setBackgroundColor(Color.TRANSPARENT)
+//            }
+//
+//            Logger.log("lightVibrantSwatch ${it?.lightVibrantSwatch}")
+//            it?.lightVibrantSwatch?.run {
+//                vibrant_light.setBackgroundColor(rgb)
+//                vibrant_light.text = rgb.toString()
+//            } ?: run {
+//                vibrant_light.setBackgroundColor(Color.TRANSPARENT)
+//            }
+//
+//            Logger.log("mutedSwatch ${it?.mutedSwatch}")
+//            it?.mutedSwatch?.run {
+//                muted.setBackgroundColor(rgb)
+//                muted.text = rgb.toString()
+//            } ?: run {
+//                muted.setBackgroundColor(Color.TRANSPARENT)
+//            }
+//
+//            Logger.log("darkMutedSwatch ${it?.darkMutedSwatch}")
+//            it?.darkMutedSwatch?.run {
+//                muted_dark.setBackgroundColor(rgb)
+//                muted_dark.text = rgb.toString()
+//            } ?: run {
+//                muted_dark.setBackgroundColor(Color.TRANSPARENT)
+//            }
+//
+//            Logger.log("lightMutedSwatch ${it?.lightMutedSwatch}")
+//            it?.lightMutedSwatch?.run {
+//                muted_light.setBackgroundColor(rgb)
+//                muted_light.text = rgb.toString()
+//            } ?: run {
+//                muted_light.setBackgroundColor(Color.TRANSPARENT)
+//            }
+//        }
+//    }
+//
+//    private fun createImage(width: Int = 50, height: Int = 50, color: Int): Bitmap {
+//        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+//        val canvas = Canvas(bitmap)
+//        val paint = Paint()
+//        paint.color = color
+//        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
+//        return bitmap
+//    }
 }
