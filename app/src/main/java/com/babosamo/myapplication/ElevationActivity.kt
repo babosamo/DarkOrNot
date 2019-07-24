ab842045dec179a3283189e05b248c2c8077d8a3
babosamo <babosamo@gmail.com>
2019-07-24 13:53:29 +0900

    color surface 추가

--- app/src/main/java/com/babosamo/myapplication/ElevationActivity.kt
+++ app/src/main/java/com/babosamo/myapplication/ElevationActivity.kt
@@ -1,12 +1,21 @@
 package com.babosamo.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class ElevationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_elevation)
+
+//        val msd1 = MaterialShapeDrawable.createWithElevationOverlay(this, 0f)
+//        val msd2 = MaterialShapeDrawable.createWithElevationOverlay(this, 8f)
+//        val msd3 = MaterialShapeDrawable.createWithElevationOverlay(this, 16f)
+//        val msd4 = MaterialShapeDrawable.createWithElevationOverlay(this, 24f)
+//        button6.background = msd1
+//        button7.background = msd2
+//        button8.background = msd3
+//        button9.background = msd4
     }
 }
